bbc014ada1094b78456ecc731a392059e7f48d80
unknown <haadirakhangi@gmail.com>
2023-03-19 00:21:57 +0530

    registration

--- api/routes/applicant/applicant.js
+++ api/routes/applicant/applicant.js
@@ -8,6 +8,14 @@ const Applicant = require("../../models/applicant/applicant")
 router.get('/login', (req, res) => {
     res.render("applicant/login")
 })
-
+router.get('/step1', (req, res) => {
+    res.render("applicant/steps/step1")
+})
+router.get('/step2', (req, res) => {
+    res.render("applicant/steps/step2")
+})
+router.get('/step3', (req, res) => {
+    res.render("applicant/steps/step3")
+})
 
 module.exports = router
