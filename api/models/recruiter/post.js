b23da7028a01a98f30012b32ce2704cd5c702a24
Hatim <hatmsb11@gmail.com>
2023-03-19 11:09:45 +0530

    scam

--- api/models/recruiter/post.js
+++ api/models/recruiter/post.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
-const recSchema = mongoose.Schema({
+const postSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     post: { type: String, required: true },
     minsal: { type: String, required: true },
     maxsal: { type: String, required: true },
     experience: { type: Number, required: true },
     location: { type: String, required: true },
-    skills: { type: String, required: true },
+    skills: [{type: mongoose.Schema.Types.ObjectId}],
 
 })
-const recr = new mongoose.model("Recruiter", recSchema)
-module.exports = recr;
+const post = new mongoose.model("post", postSchema)
+module.exports = post;
